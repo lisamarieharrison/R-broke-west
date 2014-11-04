ab7eaf224fcc95b7d4572868e14afd47e21f9655
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-11-04 14:35:33 +1100

    Lines up the adcp current at each measured location with the ctd measurements. ctd times are lined up with the closed half hour interval.

---
+++ adcp.R
@@ -0,0 +1,25 @@
+setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/Mixed models/Data/")
+adcp <- read.csv("adcp_data.csv", header = T)
+dat.cut <- read.csv("rstnCTD.csv", header= T)
+date <- read.csv("stn_coordinates.csv", header = T)
+
+dat.cut$date <- rep(date$date[date$station %in% unique(dat.cut$stn)], each = 125)
+
+stn.current <- 0
+for (i in 1:nrow(dat.cut)) {
+  #narrow find similar date and time
+  w <- which(adcp$date == unique(dat.cut$date[dat.cut$stn == dat.cut$stn[i]]) & (hours(chron(times. = adcp$time, format = "h:m:s") - unique(chron(times. = dat.cut$start.time, format = "h:m:s")[dat.cut$stn == dat.cut$stn[i]])) == 0) & (minutes(chron(times. = adcp$time, format = "h:m:s") - unique(chron(times. = dat.cut$start.time, format = "h:m:s")[dat.cut$stn == dat.cut$stn[i]])) < 20) & ((adcp$depth - dat.cut$profile.depth[i]) < 2))
+  if (length(w) > 0) {
+  stn.current[i] <- adcp$speed[w][1]
+  } else {
+    stn.current[i] <- NA
+  }
+  if (i %% 100 == 0) print(i)
+}
+stn.current[is.nan(stn.current)] <- NA
+
+dat.cut$current <- stn.current
+
+
+
+
