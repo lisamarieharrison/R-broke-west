4b25efd59cc758c6eb3c2962a54598398d72aed8
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-11-11 16:11:10 +1100

    Calculates current from ADCP data for each BROKE-West station.

--- adcp.R
+++ adcp.R
@@ -8,7 +8,7 @@ dat.cut$date <- rep(date$date[date$station %in% unique(dat.cut$stn)], each = 125
 stn.current <- 0
 for (i in 1:nrow(dat.cut)) {
   #narrow find similar date and time
-  w <- which(adcp$date == unique(dat.cut$date[dat.cut$stn == dat.cut$stn[i]]) & (hours(chron(times. = adcp$time, format = "h:m:s") - unique(chron(times. = dat.cut$start.time, format = "h:m:s")[dat.cut$stn == dat.cut$stn[i]])) == 0) & (minutes(chron(times. = adcp$time, format = "h:m:s") - unique(chron(times. = dat.cut$start.time, format = "h:m:s")[dat.cut$stn == dat.cut$stn[i]])) < 20) & ((adcp$depth - dat.cut$profile.depth[i]) < 2))
+  w <- which(adcp$date == unique(dat.cut$date[dat.cut$stn == dat.cut$stn[i]]) & (hours(chron(times. = adcp$time, format = "h:m:s") - unique(chron(times. = dat.cut$start.time, format = "h:m:s")[dat.cut$stn == dat.cut$stn[i]])) == 0) & (minutes(chron(times. = adcp$time, format = "h:m:s") - unique(chron(times. = dat.cut$start.time, format = "h:m:s")[dat.cut$stn == dat.cut$stn[i]])) < 20) & ((abs(adcp$depth - dat.cut$profile.depth[i])) < 2))
   if (length(w) > 0) {
   stn.current[i] <- adcp$speed[w][1]
   } else {
