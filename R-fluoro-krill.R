27bbc7a56a416690cfe739865fd769406a152874
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-08-24 15:36:13 +1000

    Changed to be consisted with other kaos and broke-west hurdle model scripts

--- R-fluoro-krill.R
+++ R-fluoro-krill.R
@@ -8,13 +8,14 @@ density = unlist(lapply(krill_files, read.csv))
 density[density > 5000] <- NA #remove noise
 library(lattice)
 
-
 #get glm.spl
 source("C:/Users/Lisa/Documents/phd/southern ocean/Mixed models/R code/R-broke-west/R-set-up-fluoro.R")
 
 #find stations where krill data is available and subset glm.spl
 stn <- as.numeric(gsub(".*stn_(.*)\\..*","\\1", krill_files, perl=T))
 fluoro <- glm.spl[glm.spl$stn %in% stn, ]
+fluoro <- fluoro[order(fluoro$stn), ]
+density <- density[-c(1:125)] #remove station 1 because no fluoro data
 
 #latplot of krill at each station
 krill_stn <- rep(stn, each = 125)
@@ -25,24 +26,39 @@ update(lat.plot)
 #histogram of krill density
 hist(density, main = "Histogram of krill density", xlab = "krill density (g/m2)")
 
-#plot krill against fluoro
-plot(exp(fluoro$l.obs), density, ylab = "krill density (g/m2)", pch = 19,
-     xlab = "phytoplankton fluorescence")
-title("Krill vs Phytoplankton")
-
-#plot log krill against log fluoro
-plot(log(density), fluoro$l.obs, xlab = "log krill density (g/m2)", pch = 19,
-     ylab = "log phytoplankton fluorescence")
-title("log Krill vs log Phytoplankton")
-
-#optional kernal smoothing of krill density
-density <- ksmooth(c(1:length(density)), density, bandwidth = 11)$y
-
-
-#plot krill against fluoro
-plot(fluoro$sal, log(density), ylab = "krill density (g/m2)", pch = 19)
 
 
 
+#plot log krill against environmental variables
+plot(cbind(fluoro[c(1:2, 6:9)], log(density)))
 
+#plot krill presence/absence against environmental variables
+pa <- rep(NA, length(p))
+pa[p > 0] <- 1
+pa[p == 0] <- 0
+plot(cbind(fluoro[c(1:2, 6:9)], pa))
+
+#boxplots for presence/absence
+par(mfrow = c(1, 6))
+boxplot(fluoro$oxy ~ pa)
+boxplot(fluoro$sal ~ pa)
+boxplot(fluoro$z ~ pa)
+boxplot(fluoro$par ~ pa)
+boxplot(fluoro$temp ~ pa)
+
+d <- data.frame(cbind(pa, fluoro$oxy, fluoro$sal, fluoro$z, fluoro$par, fluoro$temp, p, fluoro$stn))
+colnames(d) <- c("pa", "oxy", "sal", "z", "par", "temp", "p", "stn")
+
+pa.lm <- glm(pa ~ sal + z + temp, dat = d, family = "binomial")
+summary(pa.lm)
+
+#table of false and true 0 and 1
+table(na.omit(d)$pa, round(fitted(pa.lm)))
+
+#calculate variance inflation factors (<5 = good)
+vif(pa.lm)
+
+#calculate sensitivity and specificity
+sensitivity(as.factor(round(fitted(pa.lm))), as.factor(na.omit(d)$pa))
+specificity(as.factor(round(fitted(pa.lm))), as.factor(na.omit(d)$pa))
 
