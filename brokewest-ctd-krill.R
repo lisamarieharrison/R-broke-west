91ada4d015e42207e2a800e5b649743d717bdb38
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-08-25 15:01:57 +1000

    Not sure of changes

--- brokewest-ctd-krill.R
+++ brokewest-ctd-krill.R
@@ -12,7 +12,7 @@ file.create("brokewest_krill_ctd.csv")
 for (i in 1:length(unique(ctd$stn))) {
   
   stn <- unique(ctd$stn)[i]
-  date <- paste(ctd$year[ctd$stn == stn], ctd$month[ctd$stn == stn], ctd$day[ctd$stn == stn], sep = "")
+  date <- paste(ctd$year[ctd$stn == stn], 0 , ctd$month[ctd$stn == stn], ctd$day[ctd$stn == stn], sep = "")
 
   krill_file <- list.files("C:/Users/Lisa/Documents/phd/southern ocean/BROKE-West/Echoview/Extracted data/2x25 integration/", pattern = paste("Transect", ctd$leg[i], sep = ""), full.names = T)
   if (length(krill_file) != 0) {
@@ -42,8 +42,16 @@ for (i in 1:length(unique(ctd$stn))) {
   krill_120$Time_S <- chron(times. = krill_120$Time_S, format = "h:m:s")
   krill_120$Time_E <- chron(times. = krill_120$Time_E, format = "h:m:s")
   
-  krill_38 <- krill_38[krill_38$Time_S > (start_time - 0.02) & krill_38$Time_E < start_time, ]  
-  krill_120 <- krill_120[krill_120$Time_S > (start_time - 0.02) & krill_120$Time_E < start_time, ]  
+  krill_38 <- krill_38[krill_38$Time_S > (start_time - 0.025) & krill_38$Time_E < start_time & krill_38$Date_E == date, ]  
+  krill_120 <- krill_120[krill_120$Time_S > (start_time - 0.025) & krill_120$Time_E < start_time & krill_120$Date_E == date, ]  
+
+  #just get the closest interval
+  krill_38 <- krill_38[krill_38$Sv_mean != 9999, ]
+  krill_120 <- krill_120[krill_120$Sv_mean != 9999, ]
+  
+  krill_38 <- krill_38[krill_38$Interval %in% c(max(krill_38$Interval) - 10, max(krill_38$Interval)), ]
+  krill_120 <- krill_120[krill_120$Interval %in% c(max(krill_38$Interval) - 10, max(krill_120$Interval)), ]
+  
   
   if (nrow(krill_38) == 0) {
     next()
@@ -62,17 +70,30 @@ for (i in 1:length(unique(ctd$stn))) {
   sv_120 <- krill_120$Sv_mean
   sv_38[sv_38 > 500 | sv_38 < -500] <- NA
   sv_120[sv_120 > 500 | sv_120 < -100] <- NA
-  sv_diff <- sv_120 - sv_38
   
-  #remove 120 - 38 kHz values outside of [1.02, 14.75] because these are unlikely to be krill
-  #dB difference window is from Potts AAD report for KAOS data
-  sv_diff[sv_diff < 1.02 | sv_diff > 14.75] <- NA
-  sv_120[is.na(sv_diff)] <- NA
   
-  #convert to density using target strength (kg/m2 per interval) and average across intervals
-  p <- 2*10 ^((sv_120 - -42.22)/10)*1000
-  p[is.na(p)] <- 0
-  p <- aggregate(p, by = list(krill_38$Layer), FUN = "mean")$x  
+  sv <- 10^(sv_120/10)
+  mvbs_120 <- 10*log10(aggregate(sv, by = list(krill_38$Layer), FUN = "sum")$x/table(krill_38$Layer[1]))
+  
+  sv1_38 <- 10^(sv_38/10)
+  mvbs_38 <- 10*log10(aggregate(sv1_38, by = list(krill_38$Layer), FUN = "sum")$x/table(krill_38$Layer[1]))
+  
+  sv_diff <- mvbs_120 - mvbs_38
+    
+  mvbs_120[sv_diff < 2.5 | sv_diff > 14.7] <- NA
+  p <- 10 ^((mvbs_120 - -42.22)/10)*1000*2
+  p[is.na(mvbs_120)] <- 0
+
+  
+#   #remove 120 - 38 kHz values outside of [1.02, 14.75] because these are unlikely to be krill
+#   #dB difference window is from Potts AAD report for KAOS data
+#   sv_diff[sv_diff < 1.02 | sv_diff > 14.75] <- NA
+#   sv_120[is.na(sv_diff)] <- NA
+#   
+#   #convert to density using target strength (kg/m2 per interval) and average across intervals
+#   p <- 2*10 ^((sv_120 - -42.22)/10)*1000
+#   p[is.na(p)] <- 0
+#   p <- aggregate(p, by = list(krill_38$Layer), FUN = "mean")$x  
   
   depth <- round(aggregate(krill_38$Depth_mean, by = list(krill_38$Layer), FUN = "mean"))$x
   
