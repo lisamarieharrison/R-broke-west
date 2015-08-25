5defbb8b7d08d1e769c84d96bdfea25184941880
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-08-25 15:01:24 +1000

    Changed threshold to -90dB as recommended in literature

--- brokewest-ctd-krill-10m.R
+++ brokewest-ctd-krill-10m.R
@@ -5,6 +5,7 @@
 
 setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/BROKE-West/")
 ctd <- read.csv("C:/Users/Lisa/Documents/phd/southern ocean/Mixed models/Data/ctd_information.csv", header = T)
+ev_times <- read.csv("C:/Users/Lisa/Documents/phd/southern ocean/Mixed models/Data/ev_file_times.csv", header = T)
 library(chron)
 
 file.create("brokewest_krill_ctd.csv")
@@ -12,12 +13,25 @@ file.create("brokewest_krill_ctd.csv")
 for (i in 1:length(unique(ctd$stn))) {
   
   stn <- unique(ctd$stn)[i]
-  date <- paste(ctd$year[ctd$stn == stn], 0 , ctd$month[ctd$stn == stn], ctd$day[ctd$stn == stn], sep = "")
+  day <- ctd$day[ctd$stn == stn]
+  if (nchar(day) == 1) {
+    day <- paste(0, day, sep = "")
+  }
+  date <- paste(ctd$year[ctd$stn == stn], 0 , ctd$month[ctd$stn == stn], day, sep = "")
+  start_time <- ctd$start_time[i]
   
-  krill_file <- list.files("C:/Users/Lisa/Documents/phd/southern ocean/BROKE-West/Echoview/Extracted data/10x50 integration/", pattern = paste("Transect", ctd$leg[i], sep = ""), full.names = T)
+  while (nchar(start_time) < 6) {
+    start_time <- paste(0, start_time, sep = "")
+  }
+  
+  time <- chron(times. = as.character(start_time), format = "hms")
+  
+  f <- which(ev_times$start_date == date & chron(times. = ev_times$start_time) < time & chron(times. = ev_times$end_time) > (time - 0.05))
+    
+  krill_file <- list.files("C:/Users/Lisa/Documents/phd/southern ocean/BROKE-West/Echoview/Extracted data/10x50 integration/", full.names = T)[c(f*2, f*2 - 1)]
   if (length(krill_file) != 0) {
-    krill_120 <- read.csv(krill_file[1], header = T)
-    krill_38 <- read.csv(krill_file[2], header = T)
+    krill_120 <- read.csv(krill_file[2], header = T)
+    krill_38 <- read.csv(krill_file[1], header = T)
   } else {
     next()
   }
@@ -42,8 +56,8 @@ for (i in 1:length(unique(ctd$stn))) {
   krill_120$Time_S <- chron(times. = krill_120$Time_S, format = "h:m:s")
   krill_120$Time_E <- chron(times. = krill_120$Time_E, format = "h:m:s")
   
-  krill_38 <- krill_38[krill_38$Time_S > (start_time - 0.05) & krill_38$Time_E < start_time & krill_38$Date_E == date, ]  
-  krill_120 <- krill_120[krill_120$Time_S > (start_time - 0.05) & krill_120$Time_E < start_time & krill_120$Date_E == date, ]  
+  krill_38 <- krill_38[krill_38$Time_S > (start_time - 0.025) & krill_38$Time_E < start_time & krill_38$Date_E == date, ]  
+  krill_120 <- krill_120[krill_120$Time_S > (start_time - 0.025) & krill_120$Time_E < start_time & krill_120$Date_E == date, ]  
   
   #just get the closest interval
   krill_38 <- krill_38[abs(krill_38$Sv_mean) != 9999 & is.na(krill_38$Sv_mean) == FALSE, ]
@@ -70,15 +84,13 @@ for (i in 1:length(unique(ctd$stn))) {
   sv_38 <- krill_38$Sv_mean
   sv_120 <- krill_120$Sv_mean
   sv_38[sv_38 > 500 | sv_38 < -500] <- NA
-  sv_120[sv_120 > 500 | sv_120 < -100] <- NA
-  sv_diff <- sv_120 - sv_38
-  
+  sv_120[sv_120 > 500 | sv_120 < -90] <- NA  
   
   sv <- 10^(sv_120/10)
-  mvbs_120 <- 10*log10(aggregate(sv, by = list(krill_38$Layer), FUN = "sum")$x/table(krill_38$Layer[1]))
+  mvbs_120 <- 10*log10(aggregate(sv, by = list(krill_38$Layer), FUN = "mean", na.rm = TRUE)$x)
   
   sv1_38 <- 10^(sv_38/10)
-  mvbs_38 <- 10*log10(aggregate(sv1_38, by = list(krill_38$Layer), FUN = "sum")$x/table(krill_38$Layer[1]))
+  mvbs_38 <- 10*log10(aggregate(sv1_38, by = list(krill_38$Layer), FUN = "mean", na.rm = TRUE)$x)
   
   sv_diff <- mvbs_120 - mvbs_38
   
@@ -98,6 +110,8 @@ for (i in 1:length(unique(ctd$stn))) {
   #   p <- aggregate(p, by = list(krill_38$Layer), FUN = "mean")$x  
   
   depth <- round(aggregate(krill_38$Depth_mean, by = list(krill_38$Layer), FUN = "mean"))$x
+  
+  p[p == -Inf] <- 0
   
   out <- cbind(rep(stn, length(p)), p, depth)
   
