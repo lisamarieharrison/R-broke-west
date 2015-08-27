f8ad44d33616f82b768dfbf8d53159cbdd008010
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-08-27 11:26:42 +1000

    Added fluoro observations on natural scale to glm.spl data frame

--- R-set-up-fluoro.R
+++ R-set-up-fluoro.R
@@ -57,8 +57,8 @@ for (i in 1:n.station) {
 
 
 #data frame
-glm.spl <- data.frame(dat$l.fluoro, dat$depth, as.factor(dat$stn), rep(x, 1, each = length(unique(dat$depth))), rep(y, 1, each = length(unique(dat$depth))), dat$temp, dat$par, dat$sal, dat$oxygen, dat$ice, as.factor(dat$wm))
-names(glm.spl) <- c("l.obs", "z", "stn", "x", "y", "temp", "par", "sal", "oxy", "ice", "wm")
+glm.spl <- data.frame(dat$l.fluoro, dat$depth, as.factor(dat$stn), rep(x, 1, each = length(unique(dat$depth))), rep(y, 1, each = length(unique(dat$depth))), dat$temp, dat$par, dat$sal, dat$oxygen, dat$ice, as.factor(dat$wm), dat$fluoro)
+names(glm.spl) <- c("l.obs", "z", "stn", "x", "y", "temp", "par", "sal", "oxy", "ice", "wm", "obs")
 glm.spl$z.fact <- as.factor(as.integer(glm.spl$z))
 glm.spl$x.fact <- as.factor(glm.spl$x)
 glm.spl$y.fact <- as.factor(glm.spl$y)
